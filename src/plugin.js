757cf90a906b1e17dc49d3dadbe552fea7e9bb78
Jon Stuebe <jon@smartrent.com>
2019-07-30 13:46:10 -0700

    rendered template tags in body

--- src/plugin.js
+++ src/plugin.js
@@ -1,17 +1,17 @@
-const CryptoJS = require('crypto-js');
-const InsomniaUrl = require('insomnia-url');
-const TIMESTAMP_STORE_KEY = 'current_unixtime';
+const CryptoJS = require("crypto-js");
+const InsomniaUrl = require("insomnia-url");
+const TIMESTAMP_STORE_KEY = "current_unixtime";
 
 module.exports.templateTags = [
   {
-    name: 'sha512_hmac_auth_timestamp',
-    displayName: 'SHA512 HMAC Timestamp',
-    description: 'Current timestamp in Unixtime',
+    name: "sha512_hmac_auth_timestamp",
+    displayName: "SHA512 HMAC Timestamp",
+    description: "Current timestamp in Unixtime",
     async run(context) {
       let timestamp = await context.store.getItem(TIMESTAMP_STORE_KEY);
 
       if (!timestamp) {
-        timestamp = Math.round((new Date()).getTime() / 1000);
+        timestamp = Math.round(new Date().getTime() / 1000);
         await context.store.setItem(TIMESTAMP_STORE_KEY, timestamp);
       }
 
@@ -20,24 +20,26 @@ module.exports.templateTags = [
     }
   },
   {
-    name: 'sha512_hmac_auth_signature',
-    displayName: 'SHA512 HMAC Signature',
-    description: 'Signature of request',
+    name: "sha512_hmac_auth_signature",
+    displayName: "SHA512 HMAC Signature",
+    description: "Signature of request",
     args: [
       {
-        displayName: 'HMAC Secret',
-        type: 'string'
-      },
+        displayName: "HMAC Secret",
+        type: "string"
+      }
     ],
     async run(context, secret) {
       let timestamp = await context.store.getItem(TIMESTAMP_STORE_KEY);
 
       if (!timestamp) {
-        timestamp = Math.round((new Date()).getTime() / 1000);
+        timestamp = Math.round(new Date().getTime() / 1000);
         await context.store.setItem(TIMESTAMP_STORE_KEY, timestamp);
       }
 
-      const request = await context.util.models.request.getById(context.meta.requestId);
+      const request = await context.util.models.request.getById(
+        context.meta.requestId
+      );
       const method = request.method.toLowerCase();
       const url = new URL(await context.util.render(request.url));
       const path = url.href.substring(url.origin.length);
@@ -56,9 +58,13 @@ module.exports.templateTags = [
 
       const query = InsomniaUrl.buildQueryStringFromParams(renderedParameters);
       const fullPath = InsomniaUrl.joinUrlAndQueryString(path, query);
-      const body = request.body.text || "";
+      const body = request.body.text
+        ? await context.util.render(request.body.text)
+        : "";
       const message = timestamp + "\n" + method + "\n" + fullPath + "\n" + body;
-      const signature = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA512(message, secret));
+      const signature = CryptoJS.enc.Hex.stringify(
+        CryptoJS.HmacSHA512(message, secret)
+      );
 
       return signature;
     }
